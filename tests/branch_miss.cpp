d3706a4340834ce186b4ffe9274c80fafaa30aa3
Victor Seiji Hariki <victorseijih@gmail.com>
2017-07-02 13:33:20 -0300

    [tests] Added branch miss performance example

---
+++ tests/branch_miss.cpp
@@ -0,0 +1,67 @@
+#include <vector>
+#include <stdio.h>
+#include <ctime>
+#include <cstdlib>
+#include <algorithm>
+
+typedef unsigned long long int uli;
+typedef unsigned char byte;
+
+const uli iterations = 100000000;
+
+int main ()
+{
+    byte *vec = new byte[iterations];
+
+    printf ("Setting up vector in heap...\n");
+    srand(time(0));
+
+    for (uli i = 0; i < iterations; i++)
+        vec[i] = rand()%256;
+
+    uli tmp = 0;
+
+    // Minimizes cache misses
+    std::clock_t start_min;
+    double dur_min;
+
+    printf ("Running minimum branch miss example...\n");
+
+    start_min = std::clock();
+
+    uli sum_min = 0;
+    for (uli i = 0; i < iterations; i++)
+    {
+        if (vec[i] > 128)
+            sum_min ++;
+        else
+            sum_min --;
+    }
+
+    dur_min = (std::clock() - start_min) / (double) CLOCKS_PER_SEC;
+
+    // Maximizes cache misses
+    std::clock_t start_max;
+    double dur_max;
+
+    printf ("Sorting vector...\n");
+    std::sort(vec, vec + iterations);
+
+    printf ("Running maximum branch miss example...\n");
+
+    start_max = std::clock();
+
+    uli sum_max = 0;
+    for (uli i = 0; i < iterations; i++)
+    {
+        if (vec[i] > 128)
+            sum_max ++;
+        else
+            sum_max --;
+    }
+
+    dur_max = (std::clock() - start_max) / (double) CLOCKS_PER_SEC;
+
+    printf ("Minimum branch miss time: %lf s\n", dur_min);
+    printf ("Maximum branch miss time: %lf s\n", dur_max);
+}
