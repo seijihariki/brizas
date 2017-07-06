61b0186328fbc65395ad5159a6457c7c974ff73f
Victor Seiji Hariki <victorseijih@gmail.com>
2017-07-06 13:57:22 -0300

    [tests]Corrected branch misprediction example

--- tests/branch_misprediction.cpp
+++ tests/branch_misprediction.cpp
@@ -21,31 +21,9 @@ int main ()
 
     uli tmp = 0;
 
-    // Minimizes cache mispredictions
-    std::clock_t start_min;
-    double dur_min;
-
-    printf ("Running minimum branch misprediction example...\n");
-
-    start_min = std::clock();
-
-    uli sum_min = 0;
-    for (uli i = 0; i < iterations; i++)
-    {
-        if (vec[i] > 128)
-            sum_min ++;
-        else
-            sum_min --;
-    }
-
-    dur_min = (std::clock() - start_min) / (double) CLOCKS_PER_SEC;
-
     // Maximizes cache mispredictions
     std::clock_t start_max;
     double dur_max;
-
-    printf ("Sorting vector...\n");
-    std::sort(vec, vec + iterations);
 
     printf ("Running maximum branch misprediction example...\n");
 
@@ -61,6 +39,28 @@ int main ()
     }
 
     dur_max = (std::clock() - start_max) / (double) CLOCKS_PER_SEC;
+
+    // Minimizes cache mispredictions
+    std::clock_t start_min;
+    double dur_min;
+
+    printf ("Sorting vector...\n");
+    std::sort(vec, vec + iterations);
+
+    printf ("Running minimum branch misprediction example...\n");
+
+    start_min = std::clock();
+
+    uli sum_min = 0;
+    for (uli i = 0; i < iterations; i++)
+    {
+        if (vec[i] > 128)
+            sum_min ++;
+        else
+            sum_min --;
+    }
+
+    dur_min = (std::clock() - start_min) / (double) CLOCKS_PER_SEC;
 
     printf ("Minimum branch misprediction time: %lf s\n", dur_min);
     printf ("Maximum branch misprediction time: %lf s\n", dur_max);
